43dad045e224f625c183413e438cebf79ce92efb
RachelRebecca <RachelRebecca67@gmail.com>
2022-04-03 01:14:25 -0400

    initial commit

---
+++ Driver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSortGeneric
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] intArray = testInt(new int[] { 5, 1, 20, 8, 14, 10, 9, 13, 6, 11, 15, 18, 17, 9, 7, 12 });
+            foreach (int intIndex in intArray)
+            {
+                Console.WriteLine(intIndex);
+            }
+
+            Console.WriteLine();
+
+
+            string[] strArray = testStr(new string[] { "Beautiful!", "It all worked:)", "Amazing!", "123"});
+
+            foreach (string strIndex in strArray)
+            {
+                Console.WriteLine(strIndex);
+            }
+        }
+
+        public static int[] testInt(int[] array)
+        {
+            GenericShellSorter<int> gssInt = new GenericShellSorter<int>();
+            return gssInt.sort(array);
+        }
+
+        public static String[] testStr(string[] array)
+        {
+            GenericShellSorter<string> gssStr = new GenericShellSorter<string>();
+            return gssStr.sort(array);
+        }
+    }
+
+}
---
+++ GenericShellSorter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class GenericShellSorter<T> where T : IComparable
+{
+    public T[] sort(T[] array)
+    {
+        int nrValues = array.Length;
+        int gap = nrValues / 2;
+
+        do
+        {
+            for (int ixO = gap; ixO < nrValues; ixO++)
+            {
+                T tmp = array[ixO];
+                int ixI = ixO;
+                while (ixI >= gap && array[ixI - gap].CompareTo(tmp) > 0)
+                {
+                    array[ixI] = array[ixI - gap];
+                    ixI -= gap;
+                }
+                array[ixI] = tmp;
+            }
+            gap /= 2;
+        }
+        while (gap > 0);
+        return array;
+
+    }
+
+}
